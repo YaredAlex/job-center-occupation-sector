515a962298ac4a47e7f6eef87a3000048f966679
YaredAlex <yareda25@gmail.com>
2026-05-22 11:58:12 +0300

    add sector and subsector id when combining occupation and skill

--- sector_fetch.py
+++ sector_fetch.py
@@ -35,7 +35,32 @@ def run_query(query, variables=None):
     )
     content = response.json()
     print("="*50)
-    print(content)
+    sub_sectors_id = []
+    sectors_id = []
+    sectors = set() 
+    sub_sectors = content['data']['base_sub_sectors']
+    for sub_sector in sub_sectors:
+        sub_sectors_id.append({
+            "name":sub_sector['namejson']['en'],
+            "id":sub_sector['id']
+        })
+        sector,sector_id = sub_sector['sector']['namejson']['en'],sub_sector['sector']['id'] 
+        if sector not in sectors:
+          sectors_id.append({
+              "name":sector,
+              "id":sector_id
+          })
+          sectors.add(sector)
+    #save to json file
+    file_name = "sector_sub_sector_ids.json"
+    with open(file_name,'w') as file:
+       json.dump({
+          "sub_sectors":sub_sectors_id,
+          "sectors":sectors_id
+       },file)
+    print("="*50)
+    print(f"{file_name} saved success!")
+        
 
 run_query(SUBSECTOR_QUERY)
 
